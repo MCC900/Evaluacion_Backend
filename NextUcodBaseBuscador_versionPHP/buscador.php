804d12181dbdb8be77712fdee2c59db9667e8eec
J. Mariano Cis <mcc900@adinet.com.uy>
2018-04-05 21:38:13 -0300

    Corrección al commit anterior. Ahora si funciona la búsqueda completa.

--- NextUcodBaseBuscador_versionPHP/buscador.php
+++ NextUcodBaseBuscador_versionPHP/buscador.php
@@ -9,6 +9,7 @@
     $archivo = fopen("./data-1.json", "r");
     $data_leida = fread($archivo, filesize("./data-1.json"));
     $data = json_decode($data_leida, true);
+    $respuesta["resultadosBusqueda"] = $data;
   }
   echo json_encode($respuesta);
 
